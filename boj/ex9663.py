f97558250d071d7662c89671de5cb10ffe8923cf
ChoiWheatley <chltmdgus604@naver.com>
2023-08-12 22:17:28 +0900

    timeout(9663): N-Queen

--- boj/ex9663.py
+++ boj/ex9663.py
@@ -2,32 +2,19 @@
 8*8 체스판에 8개의 퀸을 두는 모든 경우의 수에서 시작하여 주어진 조건에 따라 가능성을 하나씩 줄여 나가는 작업을 수행해보자.
 """
 
-from typing import List
 
-
-SIDE = 8
-total = 0
-pos = [0 for _ in range(SIDE)]  # 각 열에 배치한 퀸의 위치, pos[col] = row
+SIDE = int(input())
 row_visited = [False for _ in range(SIDE)]
 
-# f(row, col) = ne_visited[row + col]
 ne_visited = [False for _ in range(SIDE * 2 - 1)]
 
-# f(row, col) = se_visited[row - col + SIDE - 1]
 se_visited = [False for _ in range(SIDE * 2 - 1)]
 
 
-def print_queens(pos: List[int]):
-    global total
-    print([(row, col) for col, row in enumerate(pos)])
-    total += 1
-
-
-def sol_recur(col: int):
-    global pos
+def sol_recur(col: int) -> int:
     if col == SIDE:
-        print_queens(pos)
-        return
+        return 1
+    count = 0
     for row in range(SIDE):
         ne_idx = row + col
         se_idx = col - row + SIDE - 1
@@ -37,15 +24,14 @@ def sol_recur(col: int):
         row_visited[row] = True
         ne_visited[ne_idx] = True
         se_visited[se_idx] = True
-        pos[col] = row
 
-        sol_recur(col + 1)
+        count += sol_recur(col + 1)
 
         # undo visit
         row_visited[row] = False
         ne_visited[ne_idx] = False
         se_visited[se_idx] = False
+    return count
 
 
-sol_recur(0)
-print(f"total count: {total}")
+print(sol_recur(0))
