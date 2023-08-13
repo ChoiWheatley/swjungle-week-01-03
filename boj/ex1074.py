b786f557a7c95d072e854b116a64d3a0e8be3fbc
ChoiWheatley <chltmdgus604@naver.com>
2023-08-13 15:12:51 +0900

    accept(1074): greater or equal

--- boj/ex1074.py
+++ boj/ex1074.py
@@ -17,21 +17,26 @@ def compare(lhs, rhs) -> Cmp:
     return Cmp.Equal
 
 
-def sol_recur(n: int, dim: int, r: int, c: int, lo: int, hi: int) -> int:
+def sol_recur(dim: int, r: int, c: int, lo: int, hi: int) -> int:
     if dim == 0:
         return lo
-    cell_area = 2 ** (2 * dim - 2)
-    cell_side = 2 ** (dim - 1)
+    # 넓이와 변의 길이를 저장한다.
+    side = 2**dim
+    area = side**2
+    # 각 분면의 넓이와 변의 길이를 저장한다.
+    cell_side = side // 2
+    cell_area = cell_side**2
+
     match (compare(r, cell_side), compare(c, cell_side)):
         case (Cmp.Less, Cmp.Less):
             # 1분면에 있음
             hi = lo + cell_area
-        case (Cmp.Less, Cmp.Greater):
+        case (Cmp.Less, Cmp.Greater | Cmp.Equal):
             # 2분면에 있음
             lo += cell_area
             hi = lo + cell_area
             c -= cell_side
-        case (Cmp.Greater, Cmp.Less):
+        case (Cmp.Greater | Cmp.Equal, Cmp.Less):
             # 3분면에 있음
             lo += cell_area * 2
             hi = lo + cell_area
@@ -42,9 +47,10 @@ def sol_recur(n: int, dim: int, r: int, c: int, lo: int, hi: int) -> int:
             hi = lo + cell_area
             r -= cell_side
             c -= cell_side
-    return sol_recur(n, dim - 1, r, c, lo, hi)
+    return sol_recur(dim - 1, r, c, lo, hi)
 
 
-n, r, c = [int(e) for e in input().split()]
+if __name__ == "__main__":
+    n, r, c = [int(e) for e in input().split()]
 
-print(sol_recur(n, n, r, c, 0, 2 ** (2 * n)))
+    print(sol_recur(n, r, c, 0, 2 ** (2 * n)))
