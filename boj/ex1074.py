e9ad13c1f237d042a01c945b71130d7be0071c57
ChoiWheatley <chltmdgus604@naver.com>
2023-08-13 14:04:56 +0900

    fail(1074): 두 번째 트라이: r, c 수정

--- boj/ex1074.py
+++ boj/ex1074.py
@@ -20,23 +20,28 @@ def compare(lhs, rhs) -> Cmp:
 def sol_recur(n: int, dim: int, r: int, c: int, lo: int, hi: int) -> int:
     if dim == 0:
         return lo
-    cell_size = 2 ** (2 * dim - 2)
-    match (compare(r, 2 ** (dim - 1)), compare(c, 2 ** (dim - 1))):
+    cell_area = 2 ** (2 * dim - 2)
+    cell_side = 2 ** (dim - 1)
+    match (compare(r, cell_side), compare(c, cell_side)):
         case (Cmp.Less, Cmp.Less):
             # 1분면에 있음
-            hi = lo + cell_size
+            hi = lo + cell_area
         case (Cmp.Less, Cmp.Greater):
             # 2분면에 있음
-            lo += cell_size
-            hi = lo + cell_size
+            lo += cell_area
+            hi = lo + cell_area
+            c -= cell_side
         case (Cmp.Greater, Cmp.Less):
             # 3분면에 있음
-            lo += cell_size * 2
-            hi = lo + cell_size
+            lo += cell_area * 2
+            hi = lo + cell_area
+            r -= cell_side
         case _:
             # 4분면에 있음
-            lo += cell_size * 3
-            hi = lo + cell_size
+            lo += cell_area * 3
+            hi = lo + cell_area
+            r -= cell_side
+            c -= cell_side
     return sol_recur(n, dim - 1, r, c, lo, hi)
 
 
